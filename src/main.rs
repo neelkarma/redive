a8c595bff93951fccfa52255e49b94072e4a7df1
Neel Sharma <nsdj.sharma@gmail.com>
2023-05-15 11:29:20 +1000

    use ureq instead of reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,24 @@
 use anyhow::{anyhow, Context, Result};
 use colored::{Color, Colorize};
 use indicatif::ProgressBar;
-use reqwest::{blocking::ClientBuilder, redirect::Policy, StatusCode, Url};
 use std::env;
+use ureq::AgentBuilder;
 
-fn get_color_from_code(status: &StatusCode) -> Color {
-    if status.is_informational() {
-        Color::Cyan
-    } else if status.is_success() {
-        Color::Green
-    } else if status.is_redirection() {
-        Color::Blue
-    } else if status.is_client_error() {
-        Color::Red
-    } else if status.is_server_error() {
-        Color::Magenta
-    } else {
-        Color::White
+fn status_to_color(status: u16) -> Color {
+    match status {
+        100..=199 => Color::Cyan,    // Information
+        200..=299 => Color::Green,   // Success
+        300..=399 => Color::Blue,    // Redirect
+        400..=499 => Color::Red,     // Client Error
+        500..=599 => Color::Magenta, // Server Error
+        _ => Color::White,
     }
 }
 
 fn main() -> Result<()> {
-    let url_str = env::args().nth(1).ok_or(anyhow!("No URL provided."))?;
+    let mut url = env::args().nth(1).ok_or(anyhow!("No URL provided."))?;
 
-    let client = ClientBuilder::new().redirect(Policy::none()).build()?;
-    let mut url = Url::parse(&url_str).context(format!("Invalid URL {}", &url_str))?;
+    let agent = AgentBuilder::new().redirects(0).build();
     let mut count = 1;
 
     let pb = ProgressBar::new_spinner();
@@ -33,31 +27,30 @@ fn main() -> Result<()> {
     loop {
         pb.set_message(format!("Tracing {}", &url));
 
-        let res = client
-            .get(url.clone())
-            .send()
-            .context(format!("Request to {} failed", &url))?;
+        let res = agent
+            .get(&url)
+            .call()
+            .context(format!("Request to {} failed. Is the URL correct?", &url))?;
 
         pb.println(format!(
             "{} {} {}",
             format!("#{}:", count).bold(),
-            format!("{}", res.status().as_u16())
+            format!("{}", res.status())
                 .bold()
-                .color(get_color_from_code(&res.status())),
-            &url.as_str().dimmed()
+                .color(status_to_color(res.status())),
+            &url.dimmed()
         ));
 
-        if !res.status().is_redirection() {
+        // Break on a non-redirect response
+        if !(300..=399).contains(&res.status()) {
             break;
         }
 
-        url = Url::parse(
-            res.headers()
-                .get("Location")
-                .context("No Location header in 3xx response")?
-                .to_str()?,
-        )
-        .context("Invalid URL in Location header")?;
+        url = res
+            .header("Location")
+            .context("No Location header in 3xx response")?
+            .to_string();
+
         count += 1;
     }
 
